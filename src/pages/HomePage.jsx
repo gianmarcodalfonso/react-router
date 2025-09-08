be69169b0ef10a7c50fbfee0c1c058219e38ac0f
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-09-08 16:58:10 +0200

    feat: add pages

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HomePage = () => {
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col-12">
+					<h1>Homepage</h1>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default HomePage;
---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProductsPage = () => {
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col-12">
+					<h1>Homepage</h1>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ProductsPage;
