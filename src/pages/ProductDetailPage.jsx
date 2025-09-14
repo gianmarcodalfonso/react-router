5b8fe71e621328f360d064baced11a7cc2ccfe33
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-09-14 23:33:22 +0200

    feat: prev&next buttons

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const ProductDetailPage = () => {
 	const { id } = useParams();
 	const [product, setProduct] = useState({});
+
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		axios.get(`https://fakestoreapi.com/products/${id}`).then((resp) => {
@@ -23,11 +25,31 @@ const ProductDetailPage = () => {
 					<div className="card">
 						<div className="card-header">{product.title}</div>
 						<div className="card-body">
-							<img src={product.image} alt={product.title} />
+							<img className="w-25" src={product.image} alt={product.title} />
 							<p>{product.category}</p>
 							<p>{product.price}</p>
 							<p>{product.description}</p>
 						</div>
+					</div>
+				</div>
+				<div className="col-12">
+					<div className="d-flex justify-content-between">
+						<button
+							className="btn btn-secondary"
+							onClick={() => {
+								navigate(`/products/${parseInt(id) - 1}`);
+							}}
+						>
+							Prev
+						</button>
+						<button
+							className="btn btn-secondary"
+							onClick={() => {
+								navigate(`/products/${parseInt(id) + 1}`);
+							}}
+						>
+							Next
+						</button>
 					</div>
 				</div>
 			</div>
